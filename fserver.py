9901d568e596e364a30b3b616adf46de4c999f83
Erik Henriksson <erik.henriksson@me.com>
2013-11-06 00:02:40 +0100

    Changed fserver to single-thread

--- fserver.py
+++ fserver.py
@@ -1,5 +1,6 @@
 from twisted.web import server, resource
 from twisted.internet import reactor, threads
+from twisted.internet.defer import Deferred
 from multiprocessing import Process, Queue
 
 import optparse
@@ -18,19 +19,20 @@ def parse_args():
 
 
 
-def f(n, q):
-    k = 1
-    for i in range(1,n+1):
-        k *= i
-    q.put(k)
-
-def MPFib(n):
-    q = Queue()
-    p = Process(target=f, args=(n,q))
-    p.start()
-    p.join()
-    return q.get()
-
+def f(n):
+    d = Deferred()
+    def _f(k, n0, nE):
+        for i in range(n0,nE+1):
+            k *= i
+        return k
+    if n <= 1000:
+        d.addCallback(_f, 1, n)
+    else:
+        d.addCallback(_f, 1, 1000)
+        for i in range(1001, n+1):
+            d.addCallback(_f, i, i+1)
+    d.callback(1)
+    return d
 
 class FacultyResource(resource.Resource):
     isLeaf = True
@@ -41,13 +43,16 @@ class FacultyResource(resource.Resource):
 
     def render_GET(self, request):
         request.setHeader("content-type", "text/plain")
-        d = threads.deferToThread(MPFib, self.n)
+        d = Deferred()
+        d.addCallback(f)
 
         def write_reply(msg):
             request.write(str(msg) + "\n")
             request.finish()
 
         d.addCallback(write_reply)
+        d.callback(self.n)
+
         return server.NOT_DONE_YET
 
 class Faculty(resource.Resource):
